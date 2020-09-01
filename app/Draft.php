cc2bdc8067631583a34be34f9547043dcf5ea577
Nedko Ivanov <ivanovnedko@gmail.com>
2020-09-01 18:15:12 +0300

    draft model and migration

---
+++ app/Draft.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Draft extends Model
+{
+
+    use SoftDeletes;
+
+    protected $fillable = ['title', 'alias', 'short_description'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -37,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function drafts()
+    {
+        return $this->hasMany('App\Draft');
+    }
 }
